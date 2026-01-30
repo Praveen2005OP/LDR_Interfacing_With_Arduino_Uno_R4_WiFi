0032a2b0711ca5fa178ad01d4f9d3a6e0bfc9dce
Praveen Kumar Sharma <sharmakarpraveen@gmail.com>
2026-01-30 10:04:38 +0530

    Enhance main.cpp with LDR sensor reading and serial output; update platformio.ini for monitor speed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <Arduino.h>
+#define LDR_PIN A0
+
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    Serial.begin(9600);
+    while (!Serial); // Important for Uno R4 WiFi
 }
 
 void loop() {
-// write your code here
+    // write your code here
+    int ldrRaw = analogRead(LDR_PIN);
+    // Map to percentage
+    int ligthtPercentage = map(ldrRaw, 0, 1023, 0, 100);
+    Serial.print("LDR Raw: ");
+    Serial.print(ldrRaw);
+    Serial.print(" | Light: ");
+    Serial.print(ligthtPercentage);
+    Serial.println("%");
+    delay(500);
 }
